34804be52bd5f050f472237c25ca11b97e67e88b
Lailaebrahim <ebrahimlaila815@gmail.com>
2023-10-19 16:10:15 -0700

    Upload Files of Task3

--- execute.c
+++ execute.c
@@ -12,7 +12,7 @@ void line_execute(char *_line, stack_t **prog_stack,
 {
 instruction_t op_arr[] = {
 			  {"push", _push}, {"pall", _pall}, {"pint", _pint},
-			  {"pop", _pop}, {NULL, NULL}
+			  {"pop", _pop}, {"swap", _swap}, {NULL, NULL}
 };
 
 unsigned int i = 0;
---
+++ swap.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+/**
+ * _swap - adds the top two elements of the stack.
+ * @head: pointer to the stack head pointer
+ * @;ine_number: Number of line
+ * Return: no return
+ */
+void _swap(stack_t **head, unsigned int line_number)
+{
+stack_t *temp = *head;
+int len = 0, value = 0;
+
+while (temp)
+{
+temp = temp->next;
+len++;
+}
+
+if (len < 2)
+{
+fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+fclose(prog_data.file);
+free(prog_data.line);
+free_stack(*head);
+exit(EXIT_FAILURE);
+}
+
+temp = *head;
+value = temp->n;
+temp->n = temp->next->n;
+temp->next->n = value;
+}
